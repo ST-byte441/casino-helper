f4e78fc0d1aa0b798d30ffcac06e941de4d6dcc9
ST-byte441 <seungtaekoh5@gmail.com>
2026-05-26 21:23:31 -0700

    feat: wire in-app update check on launch

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react'
 import { Stack } from 'expo-router'
+import Constants from 'expo-constants'
+import { checkForUpdate, UpdateInfo } from '../lib/updates'
+import UpdateModal from '../components/UpdateModal'
 
 export default function RootLayout() {
+  const [updateInfo, setUpdateInfo] = useState<UpdateInfo | null>(null)
+
+  useEffect(() => {
+    checkForUpdate().then(info => {
+      if (info?.hasUpdate) setUpdateInfo(info)
+    })
+  }, [])
+
+  const currentVersion = Constants.expoConfig?.version ?? '0.0.0'
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="blackjack/index" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="blackjack/index" options={{ headerShown: false }} />
+      </Stack>
+      {updateInfo && (
+        <UpdateModal
+          currentVersion={currentVersion}
+          latestVersion={updateInfo.latestVersion}
+          releaseUrl={updateInfo.releaseUrl}
+          onDismiss={() => setUpdateInfo(null)}
+        />
+      )}
+    </>
   )
 }
